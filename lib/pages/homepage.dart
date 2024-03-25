ee35173ec0b4cdf13e7cfd94de2cc4dc2463ece6
roseline oladiran <roselineoladiran97@gmail.com>
2024-03-25 07:40:53 -0700

    stack widget

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -21,14 +21,26 @@ class MyHomePage extends StatelessWidget {
           icon: const Icon(Icons.logout),
             )],),
         
-        body:GridView.builder(
-          itemCount: 64,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4),
-           itemBuilder: (context, index) => Container(
-            color: Colors.deepPurple,
-            margin: EdgeInsets.all(12),
-           ))
+        body:Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.deepPurple,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[400],
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.deepPurple[200],
+            )
+          ],
+        )
            
         ),
 
